65dabe71a7ebd4c52f64d7c4c6a40b741014bfff
muhammadshiva <shivamuhammad07@gmail.com>
2024-04-18 18:21:02 +0700

    add pages

--- lib/configs/pages/page.dart
+++ lib/configs/pages/page.dart
@@ -3,6 +3,9 @@ import 'package:flutter_report_app/features/forgot_password/bindings/forgot_pass
 import 'package:flutter_report_app/features/forgot_password/view/ui/forgot_password_screen.dart';
 import 'package:flutter_report_app/features/home/bindings/home_binding.dart';
 import 'package:flutter_report_app/features/home/view/ui/home_screen.dart';
+import 'package:flutter_report_app/features/production/bindings/pre_production_binding.dart';
+import 'package:flutter_report_app/features/production/view/ui/production_input_screen.dart';
+import 'package:flutter_report_app/features/production/view/ui/production_screen.dart';
 import 'package:flutter_report_app/features/sign_in/bindings/sign_in_binding.dart';
 import 'package:flutter_report_app/features/sign_in/view/ui/sign_in_screen.dart';
 import 'package:flutter_report_app/features/splash/bindings/splash_binding.dart';
@@ -34,6 +37,18 @@ abstract class Pages {
       name: Routes.homeRoute,
       page: () => const HomeScreen(),
       binding: HomeBinding(),
+    ),
+
+    GetPage(
+      name: Routes.productionRoute,
+      page: () => const ProductionScreen(),
+      binding: ProductionBinding(),
+    ),
+
+    GetPage(
+      name: Routes.productionInputRoute,
+      page: () => const ProductionInputScreen(),
+      binding: ProductionBinding(),
     ),
   ];
 }
