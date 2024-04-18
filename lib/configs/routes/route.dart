9cded07a14b729fad920b7502de276c4df49b5f3
muhammadshiva <shivamuhammad07@gmail.com>
2024-04-18 18:21:16 +0700

    add route

--- lib/configs/routes/route.dart
+++ lib/configs/routes/route.dart
@@ -3,4 +3,6 @@ abstract class Routes {
   static const String signInRoute = '/sign_in';
   static const String forgotPasswordRoute = '/forgot_password';
   static const String homeRoute = '/home';
+  static const String productionRoute = '/production';
+  static const String productionInputRoute = '/production_input';
 }
